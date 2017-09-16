f63acb39ad17791a44514a0ee381dd814a55e6bd
xzient <31088051+xzient@users.noreply.github.com>
2017-09-16 09:50:19 -0400

    Attempt add AWS SDK
    I tried to add the SDK, but do not get "IdentityManager: Got user ID: us-east-1:abcabcabc-0be6-444e-b101-abcabcabc" in the catlog

---
+++ EdUpApp/app/src/main/java/com/example/xavier/edupapp/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.xavier.edupapp;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amazonaws.mobile.config.AWSConfiguration;
+import com.amazonaws.mobile.auth.core.IdentityManager;
+import com.amazonaws.mobile.auth.core.StartupAuthResultHandler;
+import com.amazonaws.mobile.auth.core.StartupAuthResult;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Context appContext = getApplicationContext();
+        AWSConfiguration awsConfig = new AWSConfiguration(appContext);
+        IdentityManager identityManager = new IdentityManager(appContext, awsConfig);
+        IdentityManager.setDefaultIdentityManager(identityManager);
+        identityManager.doStartupAuth(this, new StartupAuthResultHandler() {
+            @Override
+            public void onComplete(StartupAuthResult startupAuthResult) {
+                // User identity is ready as unauthenticated user or previously signed-in user.
+            }
+        });
+    }
+}
